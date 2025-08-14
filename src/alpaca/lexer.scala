462339d4eb76d2db39fde8d00ebf0ddecb12abac
Bartosz Buczek <bartosz.buczek@u2i.com>
2025-08-14 21:02:28 +0200

    Lexer refacorization

--- src/alpaca/lexer.scala
+++ src/alpaca/lexer.scala
@@ -1,29 +1,31 @@
 package alpaca
 
 import scala.util.matching.Regex
+import scala.annotation.tailrec
 
-case class Token(name: String, regex: Regex, ignore: Boolean = false)
+final case class Token(name: String, regex: Regex, ignore: Boolean = false)
 
-case class Lexem(name: String, value: String, position: Int)
+final case class Lexem(name: String, value: String, position: Int)
 
-final case class Lexer(tokenDefs: List[Token]) {
-  private val compiled: Regex = tokenDefs.map { tokenDef => s"(?<${tokenDef.name}>${tokenDef.regex})" }.mkString("|").r
+final class Lexer(tokenDefs: List[Token]) {
+  private val compiled: Regex = tokenDefs.view.map { tokenDef => s"(?<${tokenDef.name}>${tokenDef.regex})" }.mkString("|").r
 
-  def tokenize(input: String, position: Int = 0): List[Lexem] = {
-    if input.isEmpty then return List.empty[Lexem]
+  @tailrec
+  def tokenize(input: String, position: Int = 0, acc: List[Lexem] = Nil): List[Lexem] = input match
+    case "" =>
+      acc.reverse
+    case _ =>
+      compiled.findPrefixMatchOf(input) match
+        case None =>
+          throw new RuntimeException(s"Unexpected character at position $position: '${input(0)}'")
 
-    compiled.findPrefixMatchOf(input) match
-      case Some(m) =>
-        val tokenDef = tokenDefs.find(token => m.group(token.name) ne null).get
-
-        if tokenDef.ignore then
-          tokenize(input.substring(m.end), position + m.end)
-        else
-          val lexem = Lexem(tokenDef.name, m.group(tokenDef.name), position)
-          lexem :: tokenize(input.substring(m.end), position + m.end)
-        end if
-
-      case None =>
-        throw new RuntimeException(s"Unexpected character at position $position: '${input(0)}'")
-  }
+        case Some(m) =>
+          tokenDefs.find(token => m.group(token.name) ne null) match
+            case Some(tokenDef) if tokenDef.ignore =>
+              tokenize(input.substring(m.end), position + m.end, acc)
+            case Some(tokenDef) =>
+              val lexem = Lexem(tokenDef.name, m.group(tokenDef.name), position)
+              tokenize(input.substring(m.end), position + m.end, lexem :: acc)
+            case None =>
+              throw new AlgorithmError(s"$m matched but no token defined for it")
 }
