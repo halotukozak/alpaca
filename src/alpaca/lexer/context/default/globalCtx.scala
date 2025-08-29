368cda3a8292c66477cadb71b6a2f97e75d70b78
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2025-08-29 15:56:40 +0200

    Refactor `Copyable` trait to extend `Function`, introduce `derived` method, and add tests for case class derivation. Annotate `lexer` and `lexerImpl` as experimental. Update context types to support `Copyable`.

--- src/alpaca/lexer/context/default/globalCtx.scala
+++ src/alpaca/lexer/context/default/globalCtx.scala
@@ -5,7 +5,6 @@ final case class EmptyGlobalCtx[LexemTpe <: Lexem[?]](
   var lastLexem: LexemTpe | Null = null,
   protected var _text: CharSequence = "",
 ) extends GlobalCtx[LexemTpe]
-// derives Copyable todo: https://github.com/halotukozak/alpaca/issues/54
 // derives Empty todo: https://github.com/halotukozak/alpaca/issues/53l
 final case class DefaultGlobalCtx[LexemTpe <: Lexem[?]](
   var lastLexem: LexemTpe | Null = null,
@@ -13,5 +12,4 @@ final case class DefaultGlobalCtx[LexemTpe <: Lexem[?]](
   var position: Int = 0,
 ) extends GlobalCtx[LexemTpe]
     with PositionTracking
-// derives Copyable todo: https://github.com/halotukozak/alpaca/issues/54
 // derives Empty todo: https://github.com/halotukozak/alpaca/issues/53l
