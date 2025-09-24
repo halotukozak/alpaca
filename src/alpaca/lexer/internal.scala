16c8da47dff6abe4e2d05bbaf1da150bed109da3
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2025-09-24 23:33:33 +0200

    Refactor `CompileNameAndPattern`: enhance regex handling, simplify pattern matching, and improve alternatives processing logic.

--- src/alpaca/lexer/internal.scala
+++ src/alpaca/lexer/internal.scala
@@ -19,24 +19,42 @@ private[lexer] final class CompileNameAndPattern[Q <: Quotes](using val quotes: 
   def apply[T: Type](pattern: Tree): List[(name: Expr[ValidName], pattern: String)] =
     @tailrec def loop(tpe: TypeRepr, pattern: Tree): List[(name: Expr[ValidName], pattern: String)] =
       (tpe, pattern) match
-        case (TermRef(qual, name), Bind(bind, tree)) if name == bind =>
-          loop(ConstantType(StringConstant(name)), tree)
-        case (tpe, Bind(_, tree)) if tpe =:= TypeRepr.of[Nothing] =>
+        // case x @ "regex" => Token[x.type]
+        case (TermRef(qual, name), Bind(bind, Literal(StringConstant(regex)))) if name == bind =>
+          List(decodeName(regex) -> regex)
+        // case x @ ("regex" | "regex2") => Token[x.type]
+        case (TermRef(qual, name), Bind(bind, Alternatives(alternatives))) if name == bind =>
+          alternatives
+            .map {
+              case Literal(StringConstant(str)) => decodeName(str) -> str
+              case x => raiseShouldNeverBeCalled(x.show)
+            }
+        // case x @ <?> => Token[<?>]
+        case (tpe, Bind(_, tree)) =>
           loop(tpe, tree)
+        // case x : "regex" => Token.Ignored
         case (tpe, Literal(StringConstant(str))) if tpe =:= TypeRepr.of[Nothing] =>
           List(decodeName(str) -> str)
+        // case x : ("regex" | "regex2") => Token.Ignored
         case (tpe, Alternatives(alternatives)) if tpe =:= TypeRepr.of[Nothing] =>
           alternatives.map {
             case Literal(StringConstant(str)) => (decodeName(str), str)
             case x => raiseShouldNeverBeCalled(x.show)
           }
+        // case x : "regex" => Token["name"]
         case (ConstantType(StringConstant(name)), Literal(StringConstant(regex))) =>
           List((decodeName(name), regex))
+        // case x : ("regex" | "regex2") => Token["name"]
         case (ConstantType(StringConstant(str)), Alternatives(alternatives)) =>
-          List(decodeName(str) -> alternatives.foldLeft("") {
-            case (acc, Literal(StringConstant(str))) => s"$acc|$str"
-            case (_, x) => raiseShouldNeverBeCalled(x.show)
-          })
+          List(
+            decodeName(str) ->
+              alternatives
+                .map {
+                  case Literal(StringConstant(str)) => str
+                  case x => raiseShouldNeverBeCalled(x.show)
+                }
+                .mkString("|"),
+          )
         case x =>
           raiseShouldNeverBeCalled(x.toString)
 
