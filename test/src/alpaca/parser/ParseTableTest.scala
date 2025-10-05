8ea5f77f4a43792c1f23464488e01cda69e3d56b
Bartosz Buczek <bartosz.buczek@u2i.com>
2025-10-05 18:55:54 +0200

    Implement conflict recognition in ParseTable and add unit tests for Shift-Reduce and Reduce-Reduce conflicts

---
+++ test/src/alpaca/parser/ParseTableTest.scala
@@ -0,0 +1,50 @@
+package alpaca.parser
+
+import alpaca.lexer.*
+import alpaca.parser.context.GlobalCtx
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import alpaca.lexer.context.Lexem
+import org.scalatest.Assertions.assertDoesNotCompile
+
+final class ParseTableTest extends AnyFunSuite with Matchers {
+  val CalcLexer = lexer {
+    case "\\+" => Token["+"]
+    case value @ "[1-9][0-9]*" => Token["Num"](value.toInt)
+  }
+
+  case class CalcContext() extends GlobalCtx
+
+  test("parse table Shift-Reduce conflict") {
+    object CalcParser extends Parser[CalcContext] {
+      val Expr: Rule[Int] =
+        case (Expr(expr1), CalcLexer.`+`(_), Expr(expr2)) => expr1 + expr2
+        case CalcLexer.Num(lexem) => lexem.value
+
+      val root: Rule[Int] =
+        case Expr(expr) => expr
+    }
+
+    assertDoesNotCompile("CalcParser.parse[Int](Nil)")
+  }
+
+  test("parse table Reduce-Reduce conflict") {
+
+    object CalcParser extends Parser[CalcContext] {
+      val Integer: Rule[Int] =
+        case CalcLexer.Num(lexem) => lexem.value
+
+      val Float: Rule[Float] =
+        case CalcLexer.Num(lexem) => lexem.value.toFloat
+
+      val Expr: Rule[Any] =
+        case Integer(value) => value
+        case Float(value) => value
+
+      val root: Rule[Any] =
+        case Expr(expr) => expr
+    }
+
+    assertDoesNotCompile("CalcParser.parse[Int](Nil)")
+  }
+}
