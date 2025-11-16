8467ace28adbce038b017f11ec53f18694dd1f76
Bartosz Buczek <bartosz.buczek@u2i.com>
2025-11-16 19:46:06 +0100

    typos

--- test/src/alpaca/integration/CompilationTheoryTest.scala
+++ test/src/alpaca/integration/CompilationTheoryTest.scala
@@ -268,14 +268,14 @@ final class CompilationTheoryTest extends AnyFunSuite:
 
   test("assignment operators, binary operators, transposition") {
     withLazyReader("""
-    C = -A;     # assignemnt with unary expression
-    C = B' ;    # assignemnt with matrix transpose
-    C = A+B ;   # assignemnt with binary addition
-    C = A-B ;   # assignemnt with binary substraction
-    C = A*B ;   # assignemnt with binary multiplication
-    C = A/B ;   # assignemnt with binary division
+    C = -A;     # assignment with unary expression
+    C = B' ;    # assignment with matrix transpose
+    C = A+B ;   # assignment with binary addition
+    C = A-B ;   # assignment with binary subtraction
+    C = A*B ;   # assignment with binary multiplication
+    C = A/B ;   # assignment with binary division
     C = A.+B ;  # add element-wise A to B
-    C = A.-B ;  # substract B from A
+    C = A.-B ;  # subtract B from A
     C = A.*B ;  # multiply element-wise A with B
     C = A./B ;  # divide element-wise A by B
 
