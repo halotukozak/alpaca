acf7a9aa7668e9cb0ea5b0b3cfc108aae31ba0d6
Bartosz Buczek <bartosz.buczek@u2i.com>
2025-12-23 14:30:14 +0100

    Comparison chapter fixes

--- thesis/src/listings/comparison/fastparse-math-parser.scala
+++ thesis/src/listings/comparison/fastparse-math-parser.scala
@@ -1,14 +1,11 @@
 object MathParser extends Parser[Int] {
   def eval(tree: (Int, Seq[(String, Int)])) = {
     val (base, ops) = tree
-    ops.foldLeft(base) { case (left, (op, right)) =>
-      op match {
-        case "+" => left + right
-        case "-" => left - right
-        case "*" => left * right
-        case "/" => left / right
-      }
-    }
+    ops.foldLeft(base):
+      case (left, ("+", right)) => left + right
+      case (left, ("-", right)) => left - right
+      case (left, ("*", right)) => left * right
+      case (left, ("/", right)) => left / right
   }
 
   def number[$: P]: P[Int] = P(CharIn("0-9").rep(1).!.map(_.toInt))
@@ -23,10 +20,9 @@ object MathParser extends Parser[Int] {
 
   def expr[$: P]: P[Int] = P(addSub ~ End)
 
-  def parse(input: String): Either[String, Int] = {
+  def parse(input: String): Either[String, Int] =
     fastparse.parse(input, expr(using _)) match {
       case Parsed.Success(value, _) => Right(value)
-      case f: Parsed.Failure        => Left(f.msg)
+      case f: Parsed.Failure => Left(f.msg)
     }
-  }
 }
