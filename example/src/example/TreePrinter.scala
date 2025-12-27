616e5e4e1f8daf2d7c06c2942acd32d5f0246e02
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2025-12-27 19:53:21 +0100

    refactor: replace `printTree` extension with `visit` for consistency across TreePrinter methods

--- example/src/example/TreePrinter.scala
+++ example/src/example/TreePrinter.scala
@@ -3,14 +3,12 @@ package example
 import example.AST.*
 
 object TreePrinter extends TreeVisitor[Int]:
-  extension (tree: AST.Tree) def printTree(indent: Int = 0): Unit = tree.visit(indent)
-
   override val handleNull: Int => Unit = _ => ()
 
   override given Process[AST.Block] = indent =>
     block =>
       println("|  " * indent + "BLOCK")
-      block.statements.foreach(_.printTree(indent + 1))
+      block.statements.foreach(_.visit(indent + 1))
 
   override given Process[AST.Literal] = indent => literal => println("|  " * indent + literal.value.toString)
 
@@ -20,67 +18,67 @@ object TreePrinter extends TreeVisitor[Int]:
     vectorRef =>
       println("|  " * indent + "VECTORREF")
       println("|  " * (indent + 1) + "ARGUMENTS")
-      vectorRef.vector.printTree(indent + 2)
-      vectorRef.element.printTree(indent + 2)
+      vectorRef.vector.visit(indent + 2)
+      vectorRef.element.visit(indent + 2)
 
   override given Process[AST.MatrixRef] = indent =>
     matrixRef =>
       println("|  " * indent + "MATRIXREF")
       println("|  " * (indent + 1) + "ARGUMENTS")
-      matrixRef.matrix.printTree(indent + 2)
-      if matrixRef.row.ne(null) then matrixRef.row.printTree(indent + 2)
-      if matrixRef.col.ne(null) then matrixRef.col.printTree(indent + 2)
+      matrixRef.matrix.visit(indent + 2)
+      if matrixRef.row.ne(null) then matrixRef.row.visit(indent + 2)
+      if matrixRef.col.ne(null) then matrixRef.col.visit(indent + 2)
 
   override given Process[AST.Apply] = indent =>
     apply =>
       println("|  " * indent + s"${apply.ref.name}")
       println("|  " * (indent + 1) + "ARGUMENTS")
-      apply.args.foreach(_.printTree(indent + 2))
+      apply.args.foreach(_.visit(indent + 2))
 
   override given Process[AST.Range] = indent =>
     range =>
       println("|  " * indent + "RANGE")
-      range.start.printTree(indent + 1)
-      range.end.printTree(indent + 1)
+      range.start.visit(indent + 1)
+      range.end.visit(indent + 1)
 
   override given Process[AST.Assign] = indent =>
     assign =>
       println("|  " * indent + "=")
-      assign.ref.printTree(indent + 1)
-      assign.expr.printTree(indent + 1)
+      assign.ref.visit(indent + 1)
+      assign.expr.visit(indent + 1)
 
   override given Process[AST.If] = indent =>
     ifStmt =>
       println("|  " * indent + "IF")
-      ifStmt.condition.printTree(indent + 1)
+      ifStmt.condition.visit(indent + 1)
       println("|  " * indent + "THEN")
-      ifStmt.thenBlock.statements.foreach(_.printTree(indent + 1))
+      ifStmt.thenBlock.statements.foreach(_.visit(indent + 1))
       if ifStmt.elseBlock.ne(null) then
         println("|  " * indent + "ELSE")
-        ifStmt.elseBlock.statements.foreach(_.printTree(indent + 1))
+        ifStmt.elseBlock.statements.foreach(_.visit(indent + 1))
 
   override given Process[AST.While] = indent =>
     whileStmt =>
       println("|  " * indent + "WHILE")
       println("|  " * (indent + 1) + "CONDITION")
-      whileStmt.condition.printTree(indent + 2)
+      whileStmt.condition.visit(indent + 2)
       println("|  " * (indent + 1) + "BODY")
-      whileStmt.body.statements.foreach(_.printTree(indent + 2))
+      whileStmt.body.statements.foreach(_.visit(indent + 2))
 
   override given Process[AST.For] = indent =>
     forStmt =>
       println("|  " * indent + "FOR")
       println("|  " * (indent + 1) + forStmt.varRef.name)
       println("|  " * (indent + 1) + "RANGE")
-      forStmt.range.start.printTree(indent + 2)
-      forStmt.range.end.printTree(indent + 2)
+      forStmt.range.start.visit(indent + 2)
+      forStmt.range.end.visit(indent + 2)
       println("|  " * (indent + 1) + "BODY")
-      forStmt.body.statements.foreach(_.printTree(indent + 2))
+      forStmt.body.statements.foreach(_.visit(indent + 2))
 
   override given Process[AST.Return] = indent =>
     returnStmt =>
       println("|  " * indent + "RETURN")
-      returnStmt.expr.printTree(indent + 1)
+      returnStmt.expr.visit(indent + 1)
 
   override given Process[AST.Continue] = indent => _ => println("|  " * indent + "CONTINUE")
 
