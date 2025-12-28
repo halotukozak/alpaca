88ee949278fd43db24a6e6dcbe00886b016ffcbc
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2025-12-28 18:42:00 +0100

    refactor: improve error handling in parser for missing -Yretain-trees flag

--- src/alpaca/internal/parser/createTables.scala
+++ src/alpaca/internal/parser/createTables.scala
@@ -118,7 +118,7 @@ private def createTablesImpl[Ctx <: ParserCtx: Type](
             ) :: others.flatten
 
   val rules = parserTpe.typeSymbol.declarations.collect:
-    case decl if decl.typeRef <:< TypeRepr.of[Rule[?]] => decl.tree
+    case decl if decl.typeRef <:< TypeRepr.of[Rule[?]] => decl.tree // todo: can we avoid .tree?
 
   report.info("Rules extracted, building parse table...")
 
@@ -126,6 +126,7 @@ private def createTablesImpl[Ctx <: ParserCtx: Type](
     .unsafeFlatMap:
       case ValDef(ruleName, _, Some(rhs)) => extractEBNF(ruleName)(rhs.asExprOf[Rule[?]])
       case DefDef(ruleName, _, _, Some(rhs)) => extractEBNF(ruleName)(rhs.asExprOf[Rule[?]]) // todo: or error?
+      case other: ValOrDefDef if other.rhs.isEmpty => report.errorAndAbort("Enable -Yretain-trees compiler flag")
     .tap: table =>
       // csv may be not the best format for this due to the commas
       debugToFile(s"$parserName/actionTable.dbg.csv")(table.toCsv)
