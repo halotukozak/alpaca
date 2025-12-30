6ec7ab8783aa4d7c70e08fe4ac418e879e157c98
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2025-12-30 20:41:37 +0100

    chore: update Scala version and Alpaca dependency; enhance symlink script with error handling

--- regenerate-symlinks.sh
+++ regenerate-symlinks.sh
@@ -1,11 +1,11 @@
 # docs
-rm README.md
+rm README.md || echo "No README.md to remove"
 ln -s ./docs/_docs/getting-started.md README.md
 # example
-rm ./example/.mill
-rm ./example/.scalafmt.conf
+rm ./example/.mill || echo "No .mill to remove"
+rm ./example/.scalafmt.conf || echo "No .scalafmt.conf to remove"
 ln -s .mill ./example/.mill
 ln -s .scalafmt.conf ./example/.scalafmt.conf
 # thesis
-rm thesis/src/alpaca.pdf
+rm thesis/src/alpaca.pdf || echo "No thesis/src/alpaca.pdf to remove"
 ln -s thesis/src/out/alpaca.pdf thesis/src/alpaca.pdf
