f969cc353e12963647c0d26f182ec32ae659dcbd
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2025-12-30 21:14:26 +0100

    regenrate symlinks

--- regenerate-symlinks.sh
+++ regenerate-symlinks.sh
@@ -1,11 +1,11 @@
 # docs
-rm -f ./README.md
-ln -s ./docs/_docs/getting-started.md ./README.md
+rm -f README.md
+ln -s docs/_docs/getting-started.md README.md
 # example
-rm -f ./example/mill
-rm -f ./example/.scalafmt.conf
-ln -s ./mill ./example/mill
-ln -s ./.scalafmt.conf ./example/.scalafmt.conf
+rm -f example/mill
+ln -s mill example/mill
+rm -f example/.scalafmt.conf
+ln -s .scalafmt.conf example/.scalafmt.conf
 # thesis
-rm -f ./thesis/src/alpaca.pdf
-ln -s out/alpaca.pdf ./thesis/src/alpaca.pdf
+rm -f thesis/src/alpaca.pdf
+ln -s out/alpaca.pdf thesis/src/alpaca.pdf
