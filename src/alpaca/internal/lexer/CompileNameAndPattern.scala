c82629db61424291f79108bdfda9812798ba3594
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2026-01-03 23:12:33 +0100

    refactor(lexer/internal): remove unused parameter in pattern matching and simplify `unsafe` logic in NonEmptyList

--- src/alpaca/internal/lexer/CompileNameAndPattern.scala
+++ src/alpaca/internal/lexer/CompileNameAndPattern.scala
@@ -33,10 +33,10 @@ private[lexer] final class CompileNameAndPattern[Q <: Quotes](using val quotes: 
       logger.trace(show"looping with tpe=$tpe and pattern=$pattern")
       (tpe, pattern) match
         // case x @ "regex" => Token[x.type]
-        case (TermRef(qual, name), Bind(bind, Literal(StringConstant(regex)))) if name == bind =>
+        case (TermRef(_, name), Bind(bind, Literal(StringConstant(regex)))) if name == bind =>
           TokenInfo.unsafe(regex, regex) :: Nil
         // case x @ ("regex" | "regex2") => Token[x.type]
-        case (TermRef(qual, name), Bind(bind, Alternatives(alternatives))) if name == bind =>
+        case (TermRef(_, name), Bind(bind, Alternatives(alternatives))) if name == bind =>
           alternatives.unsafeMap:
             case Literal(StringConstant(str)) => TokenInfo.unsafe(str, str)
         // case x @ <?> => Token[<?>]
