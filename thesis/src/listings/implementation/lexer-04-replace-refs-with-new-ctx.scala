21153a027e5cf6814918ae786b05ef764a2b586f
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2026-01-11 23:50:54 +0100

    Remove thesis sources

--- thesis/src/listings/implementation/lexer-04-replace-refs-with-new-ctx.scala
+++
@@ -1,4 +0,0 @@
-def replaceWithNewCtx(newCtx: Term) = new ReplaceRefs[quotes.type].apply(
-  (find = oldCtx.symbol, replace = newCtx),
-  (find = tree.symbol, replace = Select.unique(newCtx, "lastRawMatched")),
-)
--- thesis/src/listings/metaprogramming/meta-02-cross-stage-error.scala
+++
@@ -1,3 +0,0 @@
-def unsafeQuote(using Quotes): Expr[Int] = 
-  val localVar = 42
-  '{ localVar }  // error: access to value localVar from wrong staging level
