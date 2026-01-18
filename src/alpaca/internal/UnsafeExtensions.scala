cb08903645c80f396e72c01be7690bca02335c7e
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2026-01-19 00:27:41 +0100

    Refactor `UnsafeExtensions` to use `Log` and `DebugPosition` abstractions for improved logging and debugging.

--- src/alpaca/internal/UnsafeExtensions.scala
+++ src/alpaca/internal/UnsafeExtensions.scala
@@ -4,18 +4,15 @@ package internal
 import scala.collection.IterableOnceOps
 import scala.quoted.*
 
-inline private[alpaca] def raiseShouldNeverBeCalled[T: Default](elem: Any): T =
-  val debugSettings = compiletime.summonInline[DebugSettings]
-
+inline private[alpaca] def raiseShouldNeverBeCalled[T: Default](elem: Any)(using Log, DebugPosition): T =
   val shown =
     compiletime
       .summonFrom:
         case s: Showable[elem.type] => s
         case _ => Showable.fromToString
-      .show(elem)(using debugSettings)
+      .show(elem)
 
-  val position = compiletime.summonInline[DebugPosition].show(using debugSettings)
-  val message = show"This code should never be called: $shown at $position"
+  val message = show"This code should never be called: $shown at ${summon[DebugPosition]}"
 
   compiletime.summonFrom:
     case quotes: Quotes => quotes.reflect.report.error(message)
@@ -23,7 +20,7 @@ inline private[alpaca] def raiseShouldNeverBeCalled[T: Default](elem: Any): T =
 
   Default[T]()
 
-extension [A, CC[_], C <: IterableOnceOps[A, CC, CC[A]]](col: C)
+extension [A, CC[_], C <: IterableOnceOps[A, CC, CC[A]]](col: C)(using Log, DebugPosition)
   inline private[alpaca] def unsafeMap[B: Default](inline f: PartialFunction[A, B]): CC[B] =
     col.map(f.unsafeApply)
 
@@ -33,10 +30,10 @@ extension [A, CC[_], C <: IterableOnceOps[A, CC, CC[A]]](col: C)
   inline private[alpaca] def unsafeFoldLeft[B: Default](z: B)(inline op: PartialFunction[(B, A), B]): B =
     col.foldLeft(z)((b, a) => op.unsafeApply((b, a)))
 
-extension [A: Default](opt: Option[A])
+extension [A: Default](opt: Option[A])(using Log, DebugPosition)
   inline private[alpaca] def unsafeGet: A =
     opt.getOrElse(raiseShouldNeverBeCalled("None"))
 
-extension [A, B: Default](pf: PartialFunction[A, B])
+extension [A, B: Default](pf: PartialFunction[A, B])(using Log, DebugPosition)
   inline private[alpaca] def unsafeApply(a: A): B =
     pf.applyOrElse(a, raiseShouldNeverBeCalled)
