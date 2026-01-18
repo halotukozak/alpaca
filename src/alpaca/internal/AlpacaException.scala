5681b30ec8019e95e660af180024ae1a4977847b
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2026-01-19 00:27:30 +0100

    Introduce `AlpacaTimeoutException` as a specialized `AlpacaException`.

--- src/alpaca/internal/AlpacaException.scala
+++ src/alpaca/internal/AlpacaException.scala
@@ -3,5 +3,8 @@ package internal
 
 import scala.util.control.NoStackTrace
 
+import alpaca.internal.Showable.given_Conversion_String_Shown
 
 abstract class AlpacaException(message: Shown) extends Exception(message) with NoStackTrace
+
+object AlpacaTimeoutException extends AlpacaException("Alpaca compilation timeout")
