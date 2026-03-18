05517e1c0a353d72211b2bbb1d8e552ab521e84f
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2026-03-18 21:20:30 +0100

    refactor: remove UByte and update Brainfuck AST to use Int for memory operations

--- test/src/alpaca/integration/brainfuck/BrainAST.scala
+++ test/src/alpaca/integration/brainfuck/BrainAST.scala
@@ -11,14 +11,13 @@ enum BrainAST:
 
   def eval(mem: Memory): Unit = this match
     case BrainAST.Root(ops) => ops.foreach(_.eval(mem))
-    case BrainAST.Next => mem.pointer += 1
-    case BrainAST.Prev => mem.pointer -= 1
-    case BrainAST.Inc => mem.underlying(mem.pointer.toInt) += 1
-    case BrainAST.Dec => mem.underlying(mem.pointer.toInt) -= 1
-    case BrainAST.Print => print(mem.underlying(mem.pointer.toInt))
-    case BrainAST.Read => mem.underlying(mem.pointer.toInt) = UByte(scala.io.StdIn.readByte())
-    case BrainAST.While(ops) =>
-      while mem.underlying(mem.pointer.toInt).toInt != 0 do ops.foreach(_.eval(mem))
+    case BrainAST.Next => mem.pointer = (mem.pointer + 1) & 0xff
+    case BrainAST.Prev => mem.pointer = (mem.pointer - 1) & 0xff
+    case BrainAST.Inc => mem.underlying(mem.pointer) = (mem.underlying(mem.pointer) + 1) & 0xff
+    case BrainAST.Dec => mem.underlying(mem.pointer) = (mem.underlying(mem.pointer) - 1) & 0xff
+    case BrainAST.Print => print(mem.underlying(mem.pointer).toChar)
+    case BrainAST.Read => mem.underlying(mem.pointer) = scala.io.StdIn.readByte()
+    case BrainAST.While(ops) => while mem.underlying(mem.pointer) != 0 do ops.foreach(_.eval(mem))
     case BrainAST.FunctionDef(name, ops) => mem.functions += (name -> ops)
     case BrainAST.FunctionCall(name) =>
       mem.functions.get(name) match
@@ -26,7 +25,7 @@ enum BrainAST:
         case _ => throw RuntimeException(s"Undefined function: $name")
 
 class Memory(
-  val underlying: Array[UByte] = Array.fill(128)(UByte(0)),
-  var pointer: UByte = UByte(0),
+  val underlying: Array[Int] = new Array(255),
+  var pointer: Int = 0,
   val functions: mutable.Map[String, List[BrainAST]] = mutable.Map.empty,
 )
